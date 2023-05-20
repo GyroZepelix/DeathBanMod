88448927834e54e3a43b56d3e2a9345e447e4b3b
GyroZepelix <gamingskullgamer@gmail.com>
2023-05-20 09:18:21 +0200

    finished, working

--- src/main/java/hr/gyro/deathban/ModEvent.java
+++ src/main/java/hr/gyro/deathban/ModEvent.java
@@ -2,6 +2,7 @@ package hr.gyro.deathban;
 
 import com.mojang.authlib.GameProfile;
 import net.minecraft.ChatFormatting;
+import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
@@ -13,30 +14,20 @@ import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import java.util.Date;
-import java.util.Objects;
 import java.util.Random;
-import java.util.random.RandomGenerator;
 
 
 public class ModEvent {
-
-    private static final Random random = new Random();
     @Mod.EventBusSubscriber(modid = DeathbanMod.MOD_ID)
     public static class ForgeEvents {
         @SubscribeEvent
         public static void playerDeath(LivingDeathEvent event) {
             Entity entity = event.getEntity();
             if (entity instanceof ServerPlayer player) {
-                UserBanList userBanList = Objects.requireNonNull(player.getServer()).getPlayerList().getBans();
-                GameProfile gameProfile = player.getGameProfile();
-                Date date = new Date();
-                String banMessage = random.nextInt(50) == 0 ? "Toliko si loš da su te i roditelji ostavili Xdddddddd" : "Dosta za danas, vrijeme je za spavanac";
-                UserBanListEntry userBanListEntry = new UserBanListEntry(gameProfile, null, player.getName().getContents(), (Date)date, banMessage);
-                player.connection.disconnect(new TextComponent("You died!"));
-
-                userBanList.add(userBanListEntry);
-                player.connection.disconnect(new TextComponent(banMessage).withStyle(ChatFormatting.RED));
+                String deathMessage = event.getSource().getLocalizedDeathMessage(player).getString();
+                CommandSourceStack commandSource = player.getServer().createCommandSourceStack();
+                commandSource.sendSuccess(new TextComponent("Banning the player " + player.getName().getString() + " because he died!" ), false);
+                commandSource.getServer().getCommands().performCommand(commandSource, "ban " + player.getName().getString() + " " + deathMessage);
             }
         }
     }
